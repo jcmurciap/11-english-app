9c7b00862051f61a6c982f8d3868b2f84d92cabc
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-23 22:56:30 -0500

    refactor: change architecture of components

--- src/components/auth/SignoutScreen.js
+++ src/components/auth/SignoutScreen.js
@@ -1,95 +1,74 @@
 import React from "react";
-import './login.css';
-import { Container, Box, Typography, TextField, Button, Divider, Avatar } from '@mui/material';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import Avatar from '@mui/material/Avatar';
 import GoogleIcon from '@mui/icons-material/Google';
+import './login.css';
 
 export const SignoutScreen = () => {
     return (
-        <Container
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-            }}
-        >
+        <Container sx={{ display: "flex", justifyContent: "center" }}>
             <Box
                 sx={{
-                    boxShadow: "0px 0px 3px 3px #1976d2",
                     borderRadius: "4vh",
-                    minWidth: "290px",
-                    maxWidth: "380px",   
-                    width: "40vw",
+                    boxShadow: "5px 3px 20px #1976d2",
                     height: "auto",
-                    padding: "2vw",
                     marginTop: "5vh",
+                    maxWidth: "350px",   
+                    minWidth: "250px",
+                    p: "2vw",
                 }}
             >
                 <Typography 
-                    variant="h3" 
-                    gutterBottom 
                     component="div"
+                    gutterBottom 
                     sx={{
-                        textAlign: "center",
                         fontFamily: "'Butterfly Kids', cursive",
-                        fontSize: "8vh"
+                        fontSize: "6vh",
+                        mb: "1vh",
+                        textAlign: "center",
                     }}
+                    variant="h3" 
                 >
                     Create an account
                 </Typography>
-                <Box>
-                    <Avatar src="/broken-image.jpg" sx={{ marginLeft: "auto", marginRight: "auto", width: 60, height: 60}}/>
-                    <TextField id="outlined-basic" label="Name" variant="outlined" sx={{width: "100%", marginTop: "1vh"}}/>
-                    <TextField id="outlined-basic" label="Email" variant="outlined" sx={{width: "100%", marginTop: "1vh"}}/>
-                    <TextField 
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password" 
-                        sx={{
-                            width: "100%",
-                            marginTop: "1vh"
-                        }}
-                    />
-                    <TextField 
-                        id="outlined-password-input"
-                        label="Confirm Password"
-                        type="password"
-                        autoComplete="current-password" 
-                        sx={{
-                            width: "100%",
-                            marginTop: "1vh"
-                        }}
-                    />
-                    <Button 
-                        variant="contained" 
-                        sx={{
-                            width: "100%",
-                            marginTop: "1vh"
-                        }}
-                    >
-                        Continue
-                    </Button>
-                    <Divider
-                        sx={{
-                            marginTop: "4vh"
-                        }}
-                    >
-                        Or register with
-                    </Divider>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            marginTop: "1vh"
-                        }}
-                    >
-                        <GoogleIcon 
-                            
-                        />
-                    </Box>
-                        
-                
-                </Box>
+                <Avatar src="/broken-image.jpg" sx={{ marginX: "auto" ,width: 60, height: 60 }}/>
+                <TextField id="outlined-basic" label="Name" variant="outlined" sx={{ width: "100%", mt: "1vh" }}/>
+                <TextField id="outlined-basic" label="Email" variant="outlined" sx={{ width: "100%", mt: "1vh" }}/>
+                <TextField 
+                    autoComplete="current-password" 
+                    id="outlined-password-input"
+                    label="Password"
+                    sx={{
+                        mt: "1vh",
+                        width: "100%",
+                    }}
+                    type="password"
+                />
+                <TextField 
+                    autoComplete="current-password" 
+                    id="outlined-password-input"
+                    label="Confirm Password"
+                    sx={{
+                        mt: "1vh",
+                        width: "100%",
+                    }}
+                    type="password"
+                />
+                <Button sx={{ mt: "1vh", width: "100%" }} variant="contained">
+                    Continue
+                </Button>
+                <Divider sx={{ mt: "4vh" }}>
+                    Or register with
+                </Divider>
+                <Box sx={{ display: "flex", justifyContent: "center", mt: "1vh" }}>
+                    <GoogleIcon />
+                </Box>    
             </Box>
         </Container>
-    )
-}
+    );
+};
