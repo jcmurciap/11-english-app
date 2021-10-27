47859a70d158370ab3cd11748dab3c8566b2a4a1
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-27 16:38:07 -0500

    refactor: center content in the container

--- src/components/ui/LessonButtonsGames.js
+++ src/components/ui/LessonButtonsGames.js
@@ -1,57 +1,76 @@
 import React from 'react';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import ButtonBase from '@mui/material/ButtonBase';
 
+const Paper = styled('div')(({ theme }) => ({
+    height: "22rem",
+    [theme.breakpoints.down("sm")]: {
+        width: "50%",
+        height: "auto",
+    },
+    width: "90%",
+}));
+
+const Image = styled('img')(({ theme }) => ({
+    boxShadow: theme.shadows[3],
+    height: 150,
+    marginTop: 10,
+    maxHeight: { xs: 150, md: 200 },
+    maxWidth: { xs: 150, md: 200 },
+    minWidth: theme.breakpoints.values.sm - 350,
+    width: 200,
+}));
+
+const ContainerButtons = styled('div')(({ theme }) => ({
+    height: "auto",
+    [theme.breakpoints.down("sm")]: {
+        width: "100%",
+        height: "auto",
+    },
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 10,
+    maxHeight: { xs: 150, md: 200 },
+    maxWidth: { xs: 150, md: 200 },
+    width: 200,
+}));
+
+const ButtonGame = styled(Button)(({ theme }) => ({
+    [theme.breakpoints.down("sm")]: {
+        width: "100% !important"
+    },
+    marginBottom: 2,
+    width: 200,
+}));
 
 export const LessonButtonsGames = () => {
     return (
         <Box
             sx={{
                 display: "flex",
-                height: "22rem",
+                height: "auto",
                 justifyContent: "center",
                 mt: 1,
             }}
         >
-            <Paper
-                elevation="10"
-                sx={{ 
-                    backgroundColor: "#fefefb",
-                    width: "90%", 
-                }}
-            >
-                {/* box right/wrong words */}
-                <Box>
-                    {/* right words */}
-                    <Box></Box>
-                    {/* wrong words */}
-                    <Box></Box>
+            <Paper>
+                <Box
+                    sx={{display: "flex", justifyContent: "center"}}
+                >
+                    <Image  
+                        alt="image"
+                        src="https://d25rq8gxcq0p71.cloudfront.net/dictionary-images/324/Airplane.jpg"
+                    />
                 </Box>
-                
-                {/* Box image and options */}
-                <Box>
-                    {/* Box image */}
-                    <Box></Box>
-                    {/* Box options */}
-                    <Box>
-                        {/* option 1 */}
-                        <Box></Box>
-                        {/* option 2 */}
-                        <Box></Box>
-                        {/* option 3 */}
-                        <Box></Box>
-                        {/* option 4 */}
-                        <Box></Box>
-                    </Box>
-                </Box>
-
-                {/* score/time box */}
-                <Box>
-                    {/* score box */}
-                    <Box></Box>
-                    {/* time box */}
-                    <Box></Box>
-                </Box>
+                <ContainerButtons>
+                    <ButtonGame variant="outlined">Car</ButtonGame>
+                    <ButtonGame variant="outlined">Airplane</ButtonGame>
+                    <ButtonGame variant="outlined">Aerial Tramway</ButtonGame>
+                    <ButtonGame variant="outlined">Boat</ButtonGame>
+                    <ButtonGame color="error" size="small" variant="outlined" sx={{mt: 5, }}>Close</ButtonGame>
+                </ContainerButtons>
             </Paper>       
         </Box>
     );
