8e390fa10a93ec76f0899896649f40ffde01297d
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-27 21:42:45 -0500

    docs: add score summary of vocabulary game and change DOM architecture

--- src/components/ui/LessonButtonsGames.js
+++ src/components/ui/LessonButtonsGames.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
-import ButtonBase from '@mui/material/ButtonBase';
+import Fab from '@mui/material/Fab';
+import SportsScoreIcon from '@mui/icons-material/SportsScore';
 
 const Paper = styled('div')(({ theme }) => ({
     height: "22rem",
@@ -41,37 +43,51 @@ const ButtonGame = styled(Button)(({ theme }) => ({
     [theme.breakpoints.down("sm")]: {
         width: "100% !important"
     },
-    marginBottom: 2,
+    marginBottom: 5,
     width: 200,
 }));
 
 export const LessonButtonsGames = () => {
     return (
-        <Box
-            sx={{
-                display: "flex",
-                height: "auto",
-                justifyContent: "center",
-                mt: 1,
-            }}
+        <Grid 
+            container spacing={2} 
+            columns={7}
+            columnSpacing={{xs: 1, md: 3}}
+            mx={{ margin: 0 }} 
         >
-            <Paper>
+            <Grid item xs={6} md={6}>  
                 <Box
-                    sx={{display: "flex", justifyContent: "center"}}
+                    sx={{
+                        display: "flex",
+                        height: "auto",
+                        justifyContent: "center",
+                        mt: 1,
+                    }}
                 >
-                    <Image  
-                        alt="image"
-                        src="https://d25rq8gxcq0p71.cloudfront.net/dictionary-images/324/Airplane.jpg"
-                    />
+                    <Paper>
+                        <Box
+                            sx={{display: "flex", justifyContent: "center"}}
+                        >
+                            <Image  
+                                alt="image"
+                                src="https://d25rq8gxcq0p71.cloudfront.net/dictionary-images/324/Airplane.jpg"
+                            />
+                        </Box>
+                        <ContainerButtons>
+                            <ButtonGame variant="outlined">Car</ButtonGame>
+                            <ButtonGame variant="outlined">Airplane</ButtonGame>
+                            <ButtonGame variant="outlined">Aerial Tramway</ButtonGame>
+                            <ButtonGame variant="outlined">Boat</ButtonGame>
+                            <ButtonGame color="error" size="small" sx={{ mt: 5 }} variant="outlined">Close and Save</ButtonGame>
+                        </ContainerButtons>
+                    </Paper>
                 </Box>
-                <ContainerButtons>
-                    <ButtonGame variant="outlined">Car</ButtonGame>
-                    <ButtonGame variant="outlined">Airplane</ButtonGame>
-                    <ButtonGame variant="outlined">Aerial Tramway</ButtonGame>
-                    <ButtonGame variant="outlined">Boat</ButtonGame>
-                    <ButtonGame color="error" size="small" variant="outlined" sx={{mt: 5, }}>Close</ButtonGame>
-                </ContainerButtons>
-            </Paper>       
-        </Box>
+            </Grid>
+            <Grid item xs={1} md={1}>
+                <Fab color="primary" aria-label="scores" sx={{ mt: 5 }}>
+                    <SportsScoreIcon />
+                </Fab>         
+            </Grid>
+        </Grid>
     );
 };
